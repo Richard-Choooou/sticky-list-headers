87080287d21c6960aba6f06acda1ddd683299f10
edz <edz@edzdeMacBook-Pro-3.local>
2018-11-09 10:19:43 +0800

    修改了细节

--- examples/index.js
+++ examples/index.js
@@ -2,6 +2,6 @@ import StickyListHeaders from '../src'
 
 new StickyListHeaders({
     outerContainer: 'outer-container',
-    innerContainer: 'container',
+    innerContainer: 'inner-container',
     headers: ['header1', 'header2', 'header3', 'header4', 'header5', 'header6']
 })
